a649f42fdeab89d81ceefdcc83125bfe1ac80508
Ivan Sheyenko <ivs@sitecore.net>
2019-08-16 17:08:47 -0400

    fix

---
+++ src/Sitecore.Support.298164/DataContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.EDS.Providers.SparkPost.Data;
+using Sitecore.EDS.Providers.SparkPost.Exceptions;
+
+namespace Sitecore.Support.EDS.Providers.SparkPost.Data
+{
+    public sealed class DataContextFactory: Sitecore.EDS.Providers.SparkPost.Data.IDataContextFactory
+    {
+        public IDataContext<TEntity> CreateDataContext<TEntity>(string connectionString) where TEntity : class
+        {
+            Assert.ArgumentNotNullOrEmpty(connectionString, "connectionString");
+            try
+            {
+                return CreateLinqDataContext<TEntity>(connectionString);
+            }
+            catch (Exception innerException)
+            {
+                throw new DataException("Unable to perform data operation.", innerException);
+            }
+        }
+
+        internal  LinqDataContext<TEntity> CreateLinqDataContext<TEntity>(string connectionString) where TEntity : class
+        {
+            return new Sitecore.Support.EDS.Providers.SparkPost.Data.LinqDataContext<TEntity>(connectionString);
+        }
+    }
+}
---
+++ src/Sitecore.Support.298164/LinqDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.EDS.Providers.SparkPost.Data
+{
+    public class LinqDataContext<TEntity>: Sitecore.EDS.Providers.SparkPost.Data.LinqDataContext<TEntity> where TEntity : class
+    {
+        public LinqDataContext(string connectionString) : base(connectionString)
+        {
+            DataContext.ExecuteCommand("SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED");
+        }
+    }
+}
